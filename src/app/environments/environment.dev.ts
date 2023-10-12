9180a2bae7a3d11a966438a7a3e6cf76f7a93d28
mankuma <mankuma@cdw.com>
2023-10-12 17:44:30 +0530

    new environment files

---
+++ src/app/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    type: 'dev',
+    testRole: true,
+    authenticate: true,
+    grant_type: "implicit",
+    clientId: "a62d840c-5c2f-462a-bc69-1a45ab162f1b",
+    tenantId: "de9231de-45f4-4325-ae07-8ae72052517e",
+    doAuthToken: "jksfhdssdfugh",
+    redirectURI: "http://localhost:4200/",
+    authority: "https://login.microsoftonline.com/de9231de-45f4-4325-ae07-8ae72052517e",
+    scopes: ['user.read'],
+    uri: 'https://graph.microsoft.com/v1.0/me'
+};
